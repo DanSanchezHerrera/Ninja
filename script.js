0b76bab5f3e1cb3d990bf39b1b219e628f4a714c
DanSanchezHerrera <Daniela.a.sanchez.herrera@gmail.com>
2024-02-26 13:12:32 -0300

    [TAREA] sensei

--- script.js
+++ script.js
@@ -20,6 +20,25 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(nombre){
+        super(nombre);
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+    
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.")
+    }
+
+    statsSensei(){
+        super.showStats
+    }
+
+}
 
 const ninja1 = new Ninja("Hyabusa", 10);
 ninja1.sayName();
@@ -27,3 +46,8 @@ ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
 
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
